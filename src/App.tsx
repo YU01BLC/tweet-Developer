25668341c9bbb2024543ebc57b436c1277b914b1
YU01BLC <bgc00magenta@gmail.com>
2022-12-07 09:01:32 +0900

    update: 最上位ページのスタイルを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
-import { ProSidebarProvider } from "react-pro-sidebar";
-import { RecoilRoot } from "recoil";
-import ErrorBoundary from "../src/common/errorBoundary";
-import MainArea from "../src/page/baseComponent/mainArea";
-import SearchArea from "../src/page/baseComponent/searchArea";
-import SidebarArea from "../src/page/baseComponent/sidebarArea";
-import "../src/style/app.css";
+import { ProSidebarProvider } from 'react-pro-sidebar';
+import { RecoilRoot } from 'recoil';
+import ErrorBoundary from '../src/common/errorBoundary';
+import MainArea from '../src/page/baseComponent/mainArea';
+import SearchArea from '../src/page/baseComponent/searchArea';
+import SidebarArea from '../src/page/baseComponent/sidebarArea';
+import UserChangeModal from '../src/page/modalArea/userChangeModal';
+import '../src/style/app.css';
 
 export default function App() {
   return (
     <RecoilRoot>
       <ErrorBoundary>
         <ProSidebarProvider>
-          <div className="base-wrapper">
-            <SidebarArea />
-            <MainArea />
-            <SearchArea />
+          <div className='base-wrapper'>
+            <div className='sidebar-wrapper'>
+              <SidebarArea />
+            </div>
+            <div className='main-wrapper'>
+              <MainArea />
+              <UserChangeModal />
+            </div>
+            <div className='search-wrapper'>
+              <SearchArea />
+            </div>
           </div>
         </ProSidebarProvider>
       </ErrorBoundary>
