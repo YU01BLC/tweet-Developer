274fc8cd19107ad502ba06c17ecbc0f4038442fa
YU01BLC <bgc00magenta@gmail.com>
2022-12-20 23:36:24 +0900

    update: プロフィール情報削除処理を追加

--- src/common/deleteTimeline.ts
+++ src/common/deleteTimeline.ts
@@ -24,6 +24,28 @@ export const deleteUserInfo = (info: string) => {
     });
 };
 
+/** 初回レンダリング時にhomeで表示しているuserプロフィール情報を削除する処理 */
+export const deleteProfile = () => {
+  const collectionProfile = collection(db, 'profile_data');
+  getDocs(collectionProfile)
+    .then((querySnapshot) => {
+      const { docs } = querySnapshot;
+      (async () => {
+        await Promise.all(
+          docs.map((document) => {
+            const { id } = document;
+            return deleteDoc(doc(collectionProfile, id));
+          })
+        );
+      })().catch((error) => {
+        console.log(error);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
 /** homeで表示しているTL情報を削除する処理 */
 export const deleteTimeline = () => {
   const collectionTimeline = collection(db, 'timeline_data');
@@ -35,6 +57,28 @@ export const deleteTimeline = () => {
           docs.map((document) => {
             const { id } = document;
             return deleteDoc(doc(collectionTimeline, id));
+          })
+        );
+      })().catch((error) => {
+        console.log(error);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+/** 初回レンダリング時にhomeで表示している自分プロフィール情報を削除する処理 */
+export const deleteMyProfile = () => {
+  const collectionProfile = collection(db, 'my_profile_data');
+  getDocs(collectionProfile)
+    .then((querySnapshot) => {
+      const { docs } = querySnapshot;
+      (async () => {
+        await Promise.all(
+          docs.map((document) => {
+            const { id } = document;
+            return deleteDoc(doc(collectionProfile, id));
           })
         );
       })().catch((error) => {
